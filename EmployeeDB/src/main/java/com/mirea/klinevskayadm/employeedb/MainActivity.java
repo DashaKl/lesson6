2b2a9c840bbf96268c4da7f74651b9ac03363cc4
DashaKl <darya.klin444@gmail.com>
2023-06-01 15:39:33 +0300

    first

---
+++ EmployeeDB/src/main/java/com/mirea/klinevskayadm/employeedb/MainActivity.java
@@ -0,0 +1,48 @@
+package com.mirea.klinevskayadm.employeedb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteConstraintException;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
+
+import com.mirea.klinevskayadm.employeedb.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private String TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        AppDataBase db = App.getInstance().getDatabase();
+        EmployeeDao employeeDao = db.employeeDao();
+
+        binding.findById.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Employee employee;
+                employee = employeeDao.getById(Long.parseLong(String.valueOf(binding.id.getText())));
+                binding.Name.setText(employee.getName());
+                binding.salary.setText(employee.getSalary());
+            }
+        });
+
+        binding.save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Employee employee = new Employee();
+                employee.id = Long.parseLong(String.valueOf(binding.id.getText()));
+                employee.name = String.valueOf(binding.Name.getText());
+                employee.salary = Integer.parseInt(String.valueOf(binding.salary.getText()));
+                employeeDao.insert(employee);
+            }
+        });
+    }
+}
---
+++ SecureSharedPreferences/src/main/java/com/mirea/klinevskayadm/securesharedpreferences/MainActivity.java
@@ -0,0 +1,41 @@
+package com.mirea.klinevskayadm.securesharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKeys;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+
+import com.mirea.klinevskayadm.securesharedpreferences.databinding.ActivityMainBinding;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        KeyGenParameterSpec keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC;
+        try {
+            String mainKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec);
+            SharedPreferences secureSharedPreferences = EncryptedSharedPreferences.create(
+                    "secret_shared_prefs",
+                    mainKeyAlias,
+                    getBaseContext(),
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+            secureSharedPreferences.edit().putString("Name", "Сергей Есенин").apply();
+            binding.textView.setText(secureSharedPreferences.getString("Name", "not found"));
+        } catch (GeneralSecurityException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
